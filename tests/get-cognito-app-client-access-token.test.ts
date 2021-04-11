952fb09ead8ed1dc9f44771e19e93c3eae431bd4
David Sheffield <jeremiah.sheffield@gmail.com>
2021-04-11 14:36:06 -0400

    jest test for getting access token

---
+++ tests/get-cognito-app-client-access-token.test.ts
@@ -0,0 +1,29 @@
+import { getAppClientAccessToken } from "../aws-sdk-helpers/cognito-helper/get-app-client-access-token.util"
+import { ConsoleLog } from "../index"
+import { StsHelper } from "../index"
+import { SecretsManagerHelper } from '../index'
+
+export {}
+
+// what AWS account and role to assume before making SDK calls
+let integrationRoleToAssume = `arn:aws:iam::794776596438:role/CloudticityAdmin`
+let integrationAwsRegion = 'us-east-1'
+
+
+// jest -t 'get cognito app client access token'
+test('get cognito app client access token', async () => {
+  // asume role before using the SDK
+  await StsHelper.assumeThisRole(integrationRoleToAssume, integrationAwsRegion)
+  let secretArn = 'arn:aws:secretsmanager:us-east-1:794776596438:secret:adt-datalake-v2/acrs-cognito-app-client-MbnK0C'
+  let secretResp = await SecretsManagerHelper.getJsonFromSecret({
+    secretId: secretArn
+  })
+  // ConsoleLog.logObj(`resp`, resp)
+  let accessToken = await getAppClientAccessToken({
+    url: secretResp.token_url,
+    app_client_id: secretResp.client_id,
+    app_client_secret: secretResp.client_secret
+  })
+  ConsoleLog.logObj(`accessToken`, accessToken)
+  expect(accessToken).toBeDefined()
+}, 15000)
