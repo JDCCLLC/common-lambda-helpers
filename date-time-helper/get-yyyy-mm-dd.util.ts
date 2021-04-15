c64164c48ce730b562450c0d03be772d2bc04c3e
David Sheffield <jeremiah.sheffield@gmail.com>
2021-04-15 14:39:22 -0400

    new date helper

--- date-time-helper/get-yyyy-mm-dd.util.ts
+++ date-time-helper/get-yyyy-mm-dd.util.ts
@@ -1,3 +1,32 @@
 export function getYyyyMmDd(jsDate: Date = new Date()): string {
   return jsDate.toISOString().substring(0,10)
 }
+
+export interface YyyyMmDdHhObj {
+  year: string
+  month: string
+  day: string
+  hour: string
+}
+export function getYyyyMmDdHhAsObj(jsDate: Date = new Date()): YyyyMmDdHhObj {
+  let year = jsDate.getFullYear().toString()
+  let jsMonth = jsDate.getMonth()
+  let month = (jsMonth+1).toString()
+  if (month.length == 1) {
+    month = "0" + month
+  }
+  let day = jsDate.getDate().toString()
+  if (day.length == 1) {
+    day = "0" + day
+  }
+  let hour = jsDate.getHours().toString()
+  if (hour.length == 1) {
+    hour = "0" + hour
+  }
+  return {
+    year: year,
+    month: month,
+    day: day,
+    hour: hour
+  }
+}
--- dist/date-time-helper/get-yyyy-mm-dd.util.js
+++ dist/date-time-helper/get-yyyy-mm-dd.util.js
@@ -1,8 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getYyyyMmDd = void 0;
+exports.getYyyyMmDdHhAsObj = exports.getYyyyMmDd = void 0;
 function getYyyyMmDd(jsDate = new Date()) {
     return jsDate.toISOString().substring(0, 10);
 }
 exports.getYyyyMmDd = getYyyyMmDd;
+function getYyyyMmDdHhAsObj(jsDate = new Date()) {
+    let year = jsDate.getFullYear().toString();
+    let jsMonth = jsDate.getMonth();
+    let month = (jsMonth + 1).toString();
+    if (month.length == 1) {
+        month = "0" + month;
+    }
+    let day = jsDate.getDate().toString();
+    if (day.length == 1) {
+        day = "0" + day;
+    }
+    let hour = jsDate.getHours().toString();
+    if (hour.length == 1) {
+        hour = "0" + hour;
+    }
+    return {
+        year: year,
+        month: month,
+        day: day,
+        hour: hour
+    };
+}
+exports.getYyyyMmDdHhAsObj = getYyyyMmDdHhAsObj;
 //# sourceMappingURL=get-yyyy-mm-dd.util.js.map
