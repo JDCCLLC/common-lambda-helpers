96f77cbdeec95fd1ad79596742c7039e5945f6aa
David Sheffield <jeremiah.sheffield@gmail.com>
2021-04-25 17:49:36 -0400

    fixed math

--- dist/sleep-helper/sleep-for-seconds.util.js
+++ dist/sleep-helper/sleep-for-seconds.util.js
@@ -4,7 +4,9 @@ exports.sleepForSeconds = void 0;
 const __1 = require("..");
 async function sleepForSeconds(seconds) {
     __1.ConsoleLog.log(`sleeping for ${seconds} seconds...`);
-    return new Promise(resolve => setTimeout(resolve, (seconds)));
+    // setTimeout(function() {
+    // }, seconds)
+    return new Promise(resolve => setTimeout(resolve, (seconds * 1000)));
 }
 exports.sleepForSeconds = sleepForSeconds;
 //# sourceMappingURL=sleep-for-seconds.util.js.map
--- sleep-helper/sleep-for-seconds.util.ts
+++ sleep-helper/sleep-for-seconds.util.ts
@@ -2,5 +2,8 @@ import { ConsoleLog } from ".."
 
 export async function sleepForSeconds(seconds: number): Promise<void> {
   ConsoleLog.log(`sleeping for ${seconds} seconds...`)
-  return new Promise(resolve => setTimeout(resolve, (seconds)))
+  // setTimeout(function() {
+
+  // }, seconds)
+  return new Promise(resolve => setTimeout(resolve, (seconds*1000)))
 }
