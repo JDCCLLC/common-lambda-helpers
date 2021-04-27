e0924bd2c20f04c6468c411733a9e16d5b9c2e59
David Sheffield <jeremiah.sheffield@gmail.com>
2021-04-27 09:39:08 -0400

    get string from s3 can use credentials

--- aws-sdk-helpers/s3-helper/get-string-from-s3-object.util.ts
+++ aws-sdk-helpers/s3-helper/get-string-from-s3-object.util.ts
@@ -1,15 +1,29 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { Credentials } from "@aws-sdk/types";
 
 interface getStringFromS3Input {
   bucketName: string,
   key: string,
+  credentials?: Credentials,
+  region?: string
 }
 
 export async function getStringFromS3Object(
   props: getStringFromS3Input
 ): Promise<any> {
   // get file from s3
-  let s3Client = new S3Client({})
+  if (props.region == undefined) {
+    props.region = 'us-east-1'
+  }
+  let s3Client = new S3Client({
+    region: props.region
+  })
+  if (props.credentials != undefined) {
+    s3Client = new S3Client({
+      region: props.region,
+      credentials: props.credentials
+    })
+  }
   let getObjResp = await s3Client.send(new GetObjectCommand({
     Bucket: props.bucketName,
     Key: props.key,
